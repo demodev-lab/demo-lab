62b43d94edd6661ff5844743b468e8fd521af811
dohyeons <ddhhss0603@gmail.com>
2025-05-21 16:17:43 +0900

    feat/course-detail

--- src/app/classroom/[courseId]/page.tsx
+++ src/app/classroom/[courseId]/page.tsx
@@ -1,4 +1,9 @@
-export default function CourseDetailPage() {
-  // return <CourseDetail courseId={params.courseId} />;
-  return <div>CourseDetail 컴포넌트가 존재하지 않습니다.</div>;
+import { CourseDetail } from "@/components/course-detail";
+
+interface Props {
+  params: { courseId: string };
+}
+
+export default function CourseDetailPage({ params }: Props) {
+  return <CourseDetail courseId={params.courseId} />;
 }
